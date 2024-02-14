eaacb86185ad077f37c2be907a3b4afa182f51c5
jotavetech <ojoaogugel@gmail.com>
2024-02-14 09:29:56 -0300

    06 enums 01

---
+++ 06enums/src/main.rs
@@ -0,0 +1,35 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
+    }
+}
+
+fn main() {
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello, world!"));
+
+    m.call();
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
+
+    let teste = String::from("teste");
+}
