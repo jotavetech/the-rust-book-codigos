50ae5651677a4ac38a6e08835d5be0a635ff5680
jotavetech <ojoaogugel@gmail.com>
2024-02-13 23:31:53 -0300

    05 structs 03

--- 05rectangles-02/src/main.rs
+++ 05rectangles-02/src/main.rs
@@ -1,7 +1,24 @@
-#[derive(Debug)] // permite imprimir info de debug
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -9,15 +26,19 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!("rect1 is {:?}", rect1); // o :? diz ao compilador que queremos usar o output chamado debug, uma trait que nos permite mostrar uma strutura de uma forma util aos desenvolvedores
+    let squr1 = Rectangle::square(2);
 
-    println!(
-        "The area of the rectangle is {} square pixels",
-        area(&rect1)
-    );
-}
+    println!("{:?}", squr1);
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
