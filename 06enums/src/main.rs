28ae40e40c9f3d28a8399dfc660588cafbc8b06b
jotavetech <ojoaogugel@gmail.com>
2024-02-15 09:06:07 -0300

    06 enums 02

--- 06enums/src/main.rs
+++ 06enums/src/main.rs
@@ -19,17 +19,65 @@ impl Message {
 }
 
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
+    // let home = IpAddr::V4(127, 0, 0, 1);
+    // let loopback = IpAddr::V6(String::from("::1"));
 
-    let m = Message::Write(String::from("hello, world!"));
+    // let m = Message::Write(String::from("hello, world!"));
 
-    m.call();
+    // m.call();
 
-    let some_number = Some(5);
-    let some_char = Some('e');
+    // let some_number = Some(5);
+    // let some_char = Some('e');
 
-    let absent_number: Option<i32> = None;
+    // let absent_number: Option<i32> = None;
 
-    let teste = String::from("teste");
+    // let teste = String::from("teste");
+
+    let state = UsState::Alabama;
+
+    let coin = Coin::Quarter(state);
+
+    let value = value_in_cents(coin);
+
+    println!("{value}");
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("The state quarter from {:?}", state);
+
+            25
+        }
+    }
 }
