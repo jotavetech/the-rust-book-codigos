7b8316d8ad4547ff2d394a23d3f860b7b5661463
jotavetech <ojoaogugel@gmail.com>
2024-04-03 21:22:21 -0300

    08 common collections 03

--- 08common_collections/src/main.rs
+++ 08common_collections/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt::format;
 
 fn main() {
@@ -56,4 +57,24 @@ fn main() {
     for c in "Зд".chars() {
         println!("{c}");
     }
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    println!("{score}");
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
