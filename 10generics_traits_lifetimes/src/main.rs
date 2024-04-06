bcbb0e222ad87b31c2e118b31ee3b1cdf5a4b201
jotavetech <ojoaogugel@gmail.com>
2024-04-05 22:50:37 -0300

    09 generics, traits e lifetimes 01

---
+++ 10generics_traits_lifetimes/src/main.rs
@@ -0,0 +1,18 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+fn main() {
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 5 };
+
+    println!("integer_and_float.x = {}", integer_and_float.x());
+}
